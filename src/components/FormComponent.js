ba1f6d3a9627b2ff9374ffd98e88af12368c2ef6
om bhagat <ombhagat2003@gmail.com>
2024-07-25 12:47:50 +0530

    Initial commit

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { toast } from 'react-toastify'; // Import toast from react-toastify
+import { toast } from 'react-toastify'; 
 import { validateForm } from '../utils/validation';
 import '../styles/Form.css';
 
@@ -41,7 +41,7 @@ const FormComponent = ({ formData, setFormData, userData, setUserData }) => {
         toast.success("Data updated successfully...");
       }
       setFormData({ uname: '', uemail: '', uphone: '', umessage: '', index: '' });
-      setErrors({}); // Clear errors after successful save
+      setErrors({}); 
     }
   };
 
@@ -50,45 +50,22 @@ const FormComponent = ({ formData, setFormData, userData, setUserData }) => {
       <form onSubmit={handleSubmit}>
         <div className='form-group'>
           <label className='form-label'>Name :</label>
-          <input
-            type='text'
-            name='uname'
-            value={formData.uname}
-            onChange={getValue}
-            className='form-control'
-          />
+          <input type='text' name='uname' value={formData.uname} onChange={getValue} className='form-control'/>
           {errors.uname && <p className='error'>{errors.uname}</p>}
         </div>
         <div className='form-group'>
           <label className='form-label'>Email :</label>
-          <input
-            type='email'
-            name='uemail'
-            value={formData.uemail}
-            onChange={getValue}
-            className='form-control'
-          />
+          <input type='email' name='uemail' value={formData.uemail} onChange={getValue} className='form-control'/>
           {errors.uemail && <p className='error'>{errors.uemail}</p>}
         </div>
         <div className='form-group'>
           <label className='form-label'>Phone :</label>
-          <input
-            type='text'
-            name='uphone'
-            value={formData.uphone}
-            onChange={getValue}
-            className='form-control'
-          />
+          <input type='text' name='uphone' value={formData.uphone} onChange={getValue} className='form-control'/>
           {errors.uphone && <p className='error'>{errors.uphone}</p>}
         </div>
         <div className='form-group'>
           <label className='form-label'>Message :</label>
-          <textarea
-            name='umessage'
-            value={formData.umessage}
-            onChange={getValue}
-            className='form-control'
-          />
+          <textarea name='umessage' value={formData.umessage} onChange={getValue} className='form-control'/>
           {errors.umessage && <p className='error'>{errors.umessage}</p>}
         </div>
         <button type='submit' className='btn'>
